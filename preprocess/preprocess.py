f87803d86e1f1620125872699bc0a32f2658cb1b
wzy1935 <13053260125@163.com>
2022-03-15 00:05:51 +0800

    new commit

--- preprocess/preprocess.py
+++ preprocess/preprocess.py
@@ -20,14 +20,28 @@ class Preprocessor:
 
     @staticmethod
     def from_raw(convert_fn, file_path):
+        '''
+        从原始数据预处理生成一个Preprocessor对象
+        :param convert_fn: 处理函数
+        :param file_path: 文件位置
+        :return: 一个Preprocessor对象
+        '''
         preprocessor = Preprocessor()
         preprocessor.samples = convert_fn(file_path)
-        preprocessor.save()
         return preprocessor
 
     @staticmethod
     def load(file_path):
+        '''
+        从已处理过文件导入生成一个Preprocessor对象
+        :param file_path: 文件位置
+        :return: 一个Preprocessor对象
+        '''
         pass
 
-    def save(self):
+    def save(self, file_path):
+        '''
+        存储处理过的数据集
+        :param file_path: 文件位置
+        '''
         pass
